15faab55e8f324d4a3bfdfe89edadf3523ff7c74
Muhammad Nadeem <muhammadnadeem.1596@gmail.com>
2024-07-22 15:55:57 +0500

    worked on homepage and featured products

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,7 +1,27 @@
-import React from "react";
+import Billboard from "@/components/billboard";
+import ProductList from "@/components/product-list";
+import Container from "@/components/ui/container";
 
-const HomePage = () => {
-    return <div>HomePage</div>;
+import getBillboard from "@/actions/get-billboard";
+import getProducts from "@/actions/get-products";
+
+export const revalidate = 0;
+
+const HomePage = async () => {
+    const products = await getProducts({ isFeatured: true });
+    const billboard = await getBillboard(
+        "be2f679a-16b8-4cdc-8d2e-ca80b70d79ad",
+    );
+    return (
+        <Container>
+            <div className="space-y-10 pb-10">
+                <Billboard data={billboard} />
+                <div className="flex flex-col gap-y-8 p-4 sm:px-6 lg;px-8">
+                    <ProductList title="Featured Products" items={products} />
+                </div>
+            </div>
+        </Container>
+    );
 };
 
 export default HomePage;
